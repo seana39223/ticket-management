0848b15dca5c1b256fb6c778798237caa1be685a
Sean Anderson <sean.anderson@filament.uk.com>
2019-03-10 10:07:10 +0000

    Added in auth using laravel framework with the addition of organisation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,8 +49,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'name' => ['required', 'string', 'max:255', 'min:2'],
+            'email' => ['required', 'string', 'email', 'max:255', 'min:2', 'unique:users'],
+            'organisation' => ['required', 'string', 'max:255', 'min:2'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
@@ -66,6 +67,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'organisation' => $data['organisation'],
             'password' => Hash::make($data['password']),
         ]);
     }
